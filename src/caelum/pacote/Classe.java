0b0d12230926a8c291a3644f0a2490bda975c804
Marcel Martinelli <marcel.martinelli@mpt.gov.br>
2012-12-05 17:14:06 -0200

    segundo commit do projeto

--- src/caelum/pacote/Classe.java
+++ src/caelum/pacote/Classe.java
@@ -4,5 +4,6 @@ public class Classe {
 	
 	public static void main(String[] args) {
 		System.out.println("Exemplo de teste de uso do git");
+		System.out.println("Exemplo de teste de uso do git");
 	}
 }
